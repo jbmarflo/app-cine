166e029c5d0f124625a132f8a93ea8fa9fd651a9
Joseph Marcilla <jbmarflo@gmail.com>
2018-06-20 12:23:50 -0500

    cambiar paquete base del servlet

--- src/main/java/net/cine/app/controller/HomeController.java
+++ src/main/java/net/cine/app/controller/HomeController.java
@@ -1,4 +1,4 @@
-package net.itinajero.app.controller;
+package net.cine.app.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
